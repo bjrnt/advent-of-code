7a4ad12ec375735299753c58fe48c3c16f270286
Bjorn Tegelund <b.tegelund@gmail.com>
2022-12-11 11:46:53 -0800

    Day 11 refactor

--- day-11/src/lib.rs
+++ day-11/src/lib.rs
@@ -1,197 +1,204 @@
-use std::fmt::Display;
+use std::collections::VecDeque;
 
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{self, digit1, newline, space1},
+    character::complete::{self, multispace1},
     multi::separated_list1,
-    sequence::preceded,
-    IResult,
+    sequence::{delimited, preceded},
+    IResult, Parser,
 };
 
 #[derive(Debug)]
-enum Operation {
-    Plus(Operand, Operand),
-    Mul(Operand, Operand),
+enum Value {
+    Old,
+    Num(u64),
 }
 
-impl Operation {
-    fn apply(&self, current_old: u64) -> u64 {
+impl Value {
+    fn eval(&self, old: u64) -> u64 {
         match self {
-            Operation::Plus(op1, op2) => op1.value(current_old) + op2.value(current_old),
-            Operation::Mul(op1, op2) => op1.value(current_old) * op2.value(current_old),
+            Value::Old => old,
+            Value::Num(v) => *v,
         }
     }
 }
 
 #[derive(Debug)]
-enum Operand {
-    Old,
-    Number(u64),
+enum Operation {
+    Mul((Value, Value)),
+    Add((Value, Value)),
 }
 
-impl Operand {
-    fn value(&self, current_old: u64) -> u64 {
+impl Operation {
+    fn apply(&self, old: u64) -> u64 {
         match self {
-            Operand::Old => current_old,
-            Operand::Number(v) => *v,
+            Operation::Mul((a, b)) => a.eval(old) * b.eval(old),
+            Operation::Add((a, b)) => a.eval(old) + b.eval(old),
         }
     }
 }
 
 #[derive(Debug)]
 struct Monkey {
-    index: usize,
-    items: Vec<u64>,
+    items: VecDeque<u64>,
     operation: Operation,
-    test_divisible_by: u64,
-    if_divisible: usize,
-    if_not_divisible: usize,
+    test: Test,
+    inspect_count: u64,
 }
 
-impl Display for Monkey {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Monkey {}: {:?}", self.index, self.items)
+impl Monkey {
+    fn inspect(&mut self) -> u64 {
+        self.inspect_count += 1;
+        self.operation.apply(self.items.pop_front().unwrap())
+    }
+
+    fn test(&self, item: u64) -> u64 {
+        self.test.apply(item)
     }
 }
 
-fn parse_operation(input: &str) -> IResult<&str, Operation> {
-    let (input, operand1) = alt((digit1, tag("old")))(input)?;
-    let (input, operation) = preceded(space1, alt((tag("* "), tag("+ "))))(input)?;
-    let (input, operand2) = alt((digit1, tag("old")))(input)?;
-    let op1 = match operand1 {
-        "old" => Operand::Old,
-        s => Operand::Number(s.parse::<u64>().unwrap()),
-    };
-    let op2 = match operand2 {
-        "old" => Operand::Old,
-        s => Operand::Number(s.parse::<u64>().unwrap()),
-    };
-    let op = match operation {
-        "* " => Operation::Mul(op1, op2),
-        "+ " => Operation::Plus(op1, op2),
-        _ => panic!("unrecognized operation"),
-    };
-    Ok((input, op))
+#[derive(Debug)]
+struct Test {
+    divisible: u64,
+    true_recipient: u64,
+    false_recipient: u64,
 }
 
-fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
-    let (input, idx) = preceded(tag("Monkey "), complete::u32)(input)?;
-    let (input, _) = preceded(tag(":"), newline)(input)?;
-    let (input, starting_items) = preceded(
-        tag("  Starting items: "),
-        separated_list1(tag(", "), complete::u64),
-    )(input)?;
-    let (input, _) = newline(input)?;
-    let (input, operation) = preceded(tag("  Operation: new = "), parse_operation)(input)?;
-    let (input, _) = newline(input)?;
-    let (input, test_divisible_by) = preceded(tag("  Test: divisible by "), complete::u64)(input)?;
-    let (input, _) = newline(input)?;
-    let (input, if_divisible) =
-        preceded(tag("    If true: throw to monkey "), complete::u64)(input)?;
-    let (input, _) = newline(input)?;
-    let (input, if_not_divisible) =
-        preceded(tag("    If false: throw to monkey "), complete::u64)(input)?;
+impl Test {
+    fn apply(&self, item: u64) -> u64 {
+        if item % self.divisible == 0 {
+            self.true_recipient
+        } else {
+            self.false_recipient
+        }
+    }
+}
 
+fn value(input: &str) -> IResult<&str, Value> {
+    alt((
+        tag("old").map(|_| Value::Old),
+        complete::u64.map(Value::Num),
+    ))(input)
+}
+
+fn operation(input: &str) -> IResult<&str, Operation> {
+    let (input, value1) = preceded(tag("Operation: new = "), value)(input)?;
+    let (input, operator) = delimited(multispace1, alt((tag("*"), tag("+"))), multispace1)(input)?;
+    let (input, value2) = value(input)?;
+
+    let result = match operator {
+        "*" => Operation::Mul((value1, value2)),
+        "+" => Operation::Add((value1, value2)),
+        _ => panic!("unknown operator"),
+    };
+
+    Ok((input, result))
+}
+
+fn test_parser(input: &str) -> IResult<&str, Test> {
+    let (input, divisible) = preceded(tag("Test: divisible by "), complete::u64)(input)?;
+    let (input, true_recipient) = preceded(
+        multispace1,
+        preceded(tag("If true: throw to monkey "), complete::u64),
+    )(input)?;
+    let (input, false_recipient) = preceded(
+        multispace1,
+        preceded(tag("If false: throw to monkey "), complete::u64),
+    )(input)?;
     Ok((
         input,
-        Monkey {
-            index: idx as usize,
-            items: starting_items,
-            operation,
-            test_divisible_by,
-            if_divisible: if_divisible as usize,
-            if_not_divisible: if_not_divisible as usize,
+        Test {
+            divisible,
+            true_recipient,
+            false_recipient,
         },
     ))
 }
 
-fn parse_monkeys(input: &str) -> IResult<&str, Vec<Monkey>> {
-    let (input, monkeys) = separated_list1(tag("\n\n"), parse_monkey)(input)?;
-    Ok((input, monkeys))
+fn monkey(input: &str) -> IResult<&str, Monkey> {
+    let (input, _id) = delimited(tag("Monkey "), complete::u32, tag(":"))(input)?;
+    let (input, items) = preceded(
+        multispace1,
+        preceded(
+            tag("Starting items: "),
+            separated_list1(tag(", "), complete::u64),
+        ),
+    )(input)?;
+    let (input, op) = delimited(multispace1, operation, multispace1)(input)?;
+    let (input, test) = test_parser(input)?;
+    Ok((
+        input,
+        Monkey {
+            items: VecDeque::from(items),
+            operation: op,
+            test,
+            inspect_count: 0,
+        },
+    ))
+}
+
+fn monkeys(input: &str) -> IResult<&str, Vec<Monkey>> {
+    separated_list1(tag("\n\n"), monkey)(input)
 }
 
 pub fn process_part1(input: &str) -> String {
-    let (_, mut monkeys) = parse_monkeys(input).unwrap();
-    let mut items_inspected = vec![0; monkeys.len()];
-    let mut outbound: Vec<Vec<u64>> = vec![];
-    for _ in 0..monkeys.len() {
-        outbound.push(vec![]);
-    }
-    for _ in 1..=20 {
-        for monkey_idx in 0..monkeys.len() {
-            {
-                let monkey = &mut monkeys[monkey_idx];
-                for item_idx in 0..monkey.items.len() {
-                    let item = &monkey.items[item_idx];
-                    let new_worry_level = monkey.operation.apply(item.clone()) / 3;
-                    let is_divisible = new_worry_level % monkey.test_divisible_by == 0;
-                    items_inspected[monkey_idx] += 1;
-                    if is_divisible {
-                        outbound[monkey.if_divisible].push(new_worry_level);
-                    } else {
-                        outbound[monkey.if_not_divisible].push(new_worry_level);
-                    }
-                }
-                monkey.items.clear();
-            }
+    let (_, mut monkeys) = monkeys(input).unwrap();
 
-            for (monkey_idx, mut inbound) in outbound.iter_mut().enumerate() {
-                let items = &mut monkeys[monkey_idx].items;
-                items.append(&mut inbound);
+    for _round in 0..20 {
+        for monkey_index in 0..monkeys.len() {
+            for _ in 0..monkeys[monkey_index].items.len() {
+                let monkey = monkeys.get_mut(monkey_index).unwrap();
+                let item = monkey.inspect() / 3;
+                let monkey_to_send_to = monkey.test(item);
+                monkeys
+                    .get_mut(monkey_to_send_to as usize)
+                    .unwrap()
+                    .items
+                    .push_back(item);
             }
         }
     }
 
-    items_inspected.sort();
-    (items_inspected[items_inspected.len() - 1] * items_inspected[items_inspected.len() - 2])
+    monkeys.sort_by_key(|monkey| monkey.inspect_count);
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|monkey| monkey.inspect_count)
+        .product::<u64>()
         .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let (_, mut monkeys) = parse_monkeys(input).unwrap();
-    let mut items_inspected: Vec<u128> = vec![0; monkeys.len()];
-    let mut outbound: Vec<Vec<u64>> = vec![];
-    for _ in 0..monkeys.len() {
-        outbound.push(vec![]);
-    }
-    let magic_trick = monkeys
+    let (_, mut monkeys) = monkeys(input).unwrap();
+    let least_common_multiple = monkeys
         .iter()
-        .map(|monkey| monkey.test_divisible_by)
+        .map(|monkey| monkey.test.divisible)
         .product::<u64>();
-    dbg!(magic_trick);
-    for round in 1..=10_000 {
-        for monkey_idx in 0..monkeys.len() {
-            {
-                let monkey = &mut monkeys[monkey_idx];
-                for item_idx in 0..monkey.items.len() {
-                    let item = &monkey.items[item_idx];
-                    let new_worry_level = monkey.operation.apply(item.clone()) % magic_trick;
-                    let is_divisible = new_worry_level % monkey.test_divisible_by == 0;
-                    items_inspected[monkey_idx] += 1;
-                    if is_divisible {
-                        outbound[monkey.if_divisible].push(new_worry_level);
-                    } else {
-                        outbound[monkey.if_not_divisible].push(new_worry_level);
-                    }
-                }
-                monkey.items.clear();
-            }
 
-            for (monkey_idx, mut inbound) in outbound.iter_mut().enumerate() {
-                let items = &mut monkeys[monkey_idx].items;
-                items.append(&mut inbound);
+    for _round in 0..10_000 {
+        for monkey_index in 0..monkeys.len() {
+            for _ in 0..monkeys[monkey_index].items.len() {
+                let monkey = monkeys.get_mut(monkey_index).unwrap();
+                let item = monkey.inspect() % least_common_multiple;
+                let monkey_to_send_to = monkey.test(item);
+                monkeys
+                    .get_mut(monkey_to_send_to as usize)
+                    .unwrap()
+                    .items
+                    .push_back(item);
             }
-        }
-
-        println!("Round {round}:");
-        for monkey in monkeys.iter() {
-            println!("{}", monkey);
         }
     }
 
-    items_inspected.sort();
-    (items_inspected[items_inspected.len() - 1] * items_inspected[items_inspected.len() - 2])
+    monkeys.sort_by_key(|monkey| monkey.inspect_count);
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|monkey| monkey.inspect_count)
+        .product::<u64>()
         .to_string()
 }
 
