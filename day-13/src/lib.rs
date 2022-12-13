43d4d19f876f25224046d37cdea6c4d300217c8b
Bjorn Tegelund <b.tegelund@gmail.com>
2022-12-13 10:32:16 -0800

    Day 13 refactor

--- day-13/src/lib.rs
+++ day-13/src/lib.rs
@@ -2,115 +2,88 @@ use std::cmp::Ordering;
 
 use itertools::Itertools;
 use nom::{
+    branch::alt,
     bytes::complete::tag,
     character::complete::{self, newline},
     multi::{separated_list0, separated_list1},
-    sequence::delimited,
-    IResult,
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
 };
 
-use Node::*;
-use Ordering::*;
+use Packet::*;
 
-#[derive(Debug, Clone, PartialEq)]
-enum Node {
-    List(Vec<Node>),
+#[derive(Debug, Clone, Eq)]
+enum Packet {
+    List(Vec<Packet>),
     Value(u32),
 }
 
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (Value(l), Value(r)) => l.partial_cmp(r),
-            (ln @ List(_), rn @ Value(_)) => ln.partial_cmp(&List(vec![rn.clone()])),
-            (ln @ Value(_), rn @ List(_)) => List(vec![ln.clone()]).partial_cmp(rn),
-            (List(l), List(r)) => {
-                let content_comparison = l
-                    .into_iter()
-                    .enumerate()
-                    .filter_map(|(i, ll)| match r.get(i) {
-                        Some(rr) => ll.partial_cmp(rr),
-                        None => Some(Greater),
-                    })
-                    .find(|v| *v != Equal);
-
-                if content_comparison == Some(Equal) || content_comparison == None {
-                    if l.len() == r.len() {
-                        Some(Equal)
-                    } else {
-                        Some(Less)
-                    }
-                } else {
-                    content_comparison
-                }
-            }
+            (List(l), List(r)) => l == r,
+            (Value(l), Value(r)) => l == r,
+            (l @ Value(_), List(r)) => &vec![l.clone()] == r,
+            (List(l), r @ Value(_)) => l == &vec![r.clone()],
         }
     }
 }
 
-fn parse_node(input: &str) -> IResult<&str, Node> {
-    if input.starts_with("[") {
-        let (input, vs) =
-            delimited(tag("["), separated_list0(tag(","), parse_node), tag("]"))(input)?;
-        Ok((input, Node::List(vs)))
-    } else {
-        let (input, v) = complete::u32(input)?;
-        Ok((input, Node::Value(v)))
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
-fn parse_node_pair(input: &str) -> IResult<&str, (Node, Node)> {
-    let (input, left) = parse_node(input)?;
-    let (input, _) = newline(input)?;
-    let (input, right) = parse_node(input)?;
-    Ok((input, (left, right)))
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (List(l), List(r)) => l.cmp(r),
+            (Value(l), Value(r)) => l.cmp(r),
+            (l @ Value(_), List(r)) => vec![l.clone()].cmp(r),
+            (List(l), r @ Value(_)) => l.cmp(&vec![r.clone()]),
+        }
+    }
 }
 
-fn parse_node_pairs(input: &str) -> IResult<&str, Vec<(Node, Node)>> {
-    let (input, node_pairs) = separated_list1(tag("\n\n"), parse_node_pair)(input)?;
-    Ok((input, node_pairs))
+fn parse_packet(input: &str) -> IResult<&str, Packet> {
+    alt((
+        delimited(tag("["), separated_list0(tag(","), parse_packet), tag("]")).map(List),
+        complete::u32.map(Value),
+    ))(input)
+}
+
+fn parse_packet_pairs(input: &str) -> IResult<&str, Vec<(Packet, Packet)>> {
+    separated_list1(
+        tag("\n\n"),
+        separated_pair(parse_packet, newline, parse_packet),
+    )(input)
 }
 
 pub fn process_part1(input: &str) -> String {
-    let (_, node_pairs) = parse_node_pairs(input).unwrap();
-    node_pairs
+    let (_, packet_pairs) = parse_packet_pairs(input).unwrap();
+    packet_pairs
         .iter()
         .enumerate()
-        .filter_map(
-            |(i, (left, right))| {
-                if left < right {
-                    Some(i + 1)
-                } else {
-                    None
-                }
-            },
-        )
+        .filter_map(|(i, (left, right))| (left < right).then_some(i + 1))
         .sum::<usize>()
         .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let (_, mut node_pairs) = parse_node_pairs(input).unwrap();
+    let (_, packet_pairs) = parse_packet_pairs(input).unwrap();
 
-    let tracer1 = Node::List(vec![Node::List(vec![Node::Value(2)])]);
-    let tracer2 = Node::List(vec![Node::List(vec![Node::Value(6)])]);
-    node_pairs.push((tracer1.clone(), tracer2.clone()));
+    let tracer1 = List(vec![List(vec![Value(2)])]);
+    let tracer2 = List(vec![List(vec![Value(6)])]);
 
-    let nodes = node_pairs
+    let mut packets = packet_pairs
         .into_iter()
-        .flat_map(|(left, right)| [left, right])
-        .sorted_by(|left, right| left.partial_cmp(right).unwrap())
-        .enumerate();
+        .chain([(tracer1.clone(), tracer2.clone())])
+        .flat_map(|(l, r)| [l, r])
+        .sorted();
 
-    let (tracer1_idx, _) = nodes
-        .clone()
-        .find(|(_, n)| tracer1.partial_cmp(n) == Some(Ordering::Equal))
-        .unwrap();
-
-    let (tracer2_idx, _) = nodes
-        .clone()
-        .find(|(_, n)| tracer2.partial_cmp(n) == Some(Ordering::Equal))
-        .unwrap();
+    let tracer1_idx = packets.clone().position(|p| tracer1 == p).unwrap();
+    let tracer2_idx = packets.position(|p| tracer2 == p).unwrap();
 
     ((tracer1_idx + 1) * (tracer2_idx + 1)).to_string()
 }
