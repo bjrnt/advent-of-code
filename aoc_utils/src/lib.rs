eda33b136a61a86f98cda6e39c4e160a2c96b022
Bjorn Tegelund <b.tegelund@gmail.com>
2022-12-20 17:13:24 -0800

    Add utils package

---
+++ aoc_utils/src/lib.rs
@@ -0,0 +1,127 @@
+use std::collections::VecDeque;
+
+trait TraveralStore<S>: Extend<S> {
+    fn next_state(&mut self) -> Option<S>;
+}
+
+impl<S> TraveralStore<S> for Vec<S> {
+    fn next_state(&mut self) -> Option<S> {
+        self.pop()
+    }
+}
+
+impl<S> TraveralStore<S> for VecDeque<S> {
+    fn next_state(&mut self) -> Option<S> {
+        self.pop_front()
+    }
+}
+
+#[inline]
+fn traverser<S, F, G>(mut ts: impl TraveralStore<S>, mut next_states_fn: F, mut interrupt_fn: G)
+where
+    F: FnMut(S) -> Option<Vec<S>>,
+    G: FnMut(&S) -> bool,
+{
+    while let Some(state) = ts.next_state() {
+        if interrupt_fn(&state) {
+            return;
+        }
+
+        if let Some(next_states) = next_states_fn(state) {
+            ts.extend(next_states);
+        }
+    }
+}
+
+#[inline]
+pub fn complete_bfs<S, F>(initial_states: impl Iterator<Item = S>, next_states_fn: F)
+where
+    F: FnMut(S) -> Option<Vec<S>>,
+{
+    interruptable_bfs(initial_states, next_states_fn, |_| false);
+}
+
+#[inline]
+pub fn interruptable_bfs<S, F, G>(
+    initial_states: impl Iterator<Item = S>,
+    next_states_fn: F,
+    terminal_state_fn: G,
+) where
+    F: FnMut(S) -> Option<Vec<S>>,
+    G: FnMut(&S) -> bool,
+{
+    traverser(
+        VecDeque::from_iter(initial_states),
+        next_states_fn,
+        terminal_state_fn,
+    )
+}
+
+#[inline]
+pub fn complete_dfs<S, F>(initial_states: impl Iterator<Item = S>, next_states_fn: F)
+where
+    F: FnMut(S) -> Option<Vec<S>>,
+{
+    interruptable_dfs(initial_states, next_states_fn, |_| false)
+}
+
+#[inline]
+pub fn interruptable_dfs<S, F, G>(
+    initial_states: impl Iterator<Item = S>,
+    next_states_fn: F,
+    terminal_state_fn: G,
+) where
+    F: FnMut(S) -> Option<Vec<S>>,
+    G: FnMut(&S) -> bool,
+{
+    traverser(
+        Vec::from_iter(initial_states),
+        next_states_fn,
+        terminal_state_fn,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_bfs_init() {
+        let mut invocations = 0;
+        let initial_states = [0, 1, 2];
+        complete_bfs(initial_states.iter(), |_| {
+            invocations += 1;
+            None
+        });
+        assert_eq!(invocations, 3);
+    }
+
+    #[test]
+    fn basic_bfs_neighbors() {
+        let mut invocations = 0;
+        let initial_states = [0];
+        complete_bfs(initial_states.into_iter(), |n| {
+            assert_eq!(n, invocations);
+            invocations += 1;
+            if n < 5 {
+                Some(vec![n + 1])
+            } else {
+                None
+            }
+        });
+    }
+
+    #[test]
+    fn interrupted_bfs() {
+        let mut invocations = 0;
+        interruptable_bfs(
+            [0].into_iter(),
+            |n| {
+                invocations += 1;
+                Some(vec![n + 1])
+            },
+            |n| *n > 3,
+        );
+        assert_eq!(4, invocations);
+    }
+}
