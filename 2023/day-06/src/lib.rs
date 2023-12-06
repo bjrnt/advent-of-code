cf54d4b3d1d2f9ff3309ed54386ef16f1eed7482
Bjorn Tegelund <tegelund@amazon.com>
2023-12-06 14:50:36 -0800

    Day 6 analytical

--- 2023/day-06/src/lib.rs
+++ 2023/day-06/src/lib.rs
@@ -3,18 +3,18 @@ use nom::character::complete::{newline, space1};
 use nom::multi::separated_list1;
 use nom::IResult;
 
-fn parse_races(input: &str) -> IResult<&str, Vec<(u64, u64)>> {
+fn parse_races(input: &str) -> IResult<&str, Vec<(f64, f64)>> {
     let (input, _) = tag("Time: ")(input)?;
     let (input, _) = space1(input)?;
-    let (input, times) = separated_list1(space1, nom::character::complete::u64)(input)?;
+    let (input, times) = separated_list1(space1, nom::number::complete::double)(input)?;
     let (input, _) = newline(input)?;
     let (input, _) = tag("Distance: ")(input)?;
     let (input, _) = space1(input)?;
-    let (input, distances) = separated_list1(space1, nom::character::complete::u64)(input)?;
+    let (input, distances) = separated_list1(space1, nom::number::complete::double)(input)?;
     Ok((input, times.into_iter().zip(distances).collect()))
 }
 
-fn parse_single_race(input: &str) -> (u64, u64) {
+fn parse_single_race(input: &str) -> (f64, f64) {
     let numbers_in_lines = input
         .lines()
         .map(|line| {
@@ -29,8 +29,17 @@ fn parse_single_race(input: &str) -> (u64, u64) {
     )
 }
 
-fn calculate_distance(total_time: u64, charge_for: u64) -> u64 {
-    (total_time - charge_for) * charge_for
+fn ways_to_win_race(total_time: f64, distance_to_beat: f64) -> f64 {
+    let d = (total_time * total_time - 4f64 * distance_to_beat).sqrt();
+    let mut root2 = (-total_time - d) / -2f64;
+    let mut root1 = (-total_time + d) / -2f64;
+    if root1.ceil() == root1 {
+        root1 += 1f64;
+    }
+    if root2.floor() == root2 {
+        root2 -= 1f64;
+    }
+    root2.floor() - root1.ceil() + 1f64
 }
 
 pub fn process_part1(input: &str) -> String {
@@ -38,20 +47,15 @@ pub fn process_part1(input: &str) -> String {
     races
         .into_iter()
         .map(|(total_time, distance_to_beat)| {
-            (1..total_time)
-                .filter(|charge_for| calculate_distance(total_time, *charge_for) > distance_to_beat)
-                .count()
+            ways_to_win_race(total_time, distance_to_beat)
         })
-        .product::<usize>()
+        .product::<f64>()
         .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
     let (total_time, distance_to_beat) = parse_single_race(input);
-    (1..total_time)
-        .filter(|charge_for| calculate_distance(total_time, *charge_for) > distance_to_beat)
-        .count()
-        .to_string()
+    ways_to_win_race(total_time, distance_to_beat).to_string()
 }
 
 #[cfg(test)]
