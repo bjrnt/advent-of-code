82bdaee6cba6f462a43a2d17362d86743519f621
Bjorn Tegelund <b.tegelund@gmail.com>
2023-12-12 19:53:57 -0800

    Day 12 part 2

--- 2023/day-12/src/lib.rs
+++ 2023/day-12/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use nom::{
     branch::alt,
@@ -7,17 +9,16 @@ use nom::{
     sequence::separated_pair,
     IResult, Parser,
 };
-use rayon::prelude::*;
 use Record::*;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Record {
     Operational,
     Damaged,
     Unknown,
 }
 
-fn parse_row(input: &str) -> IResult<&str, (Vec<Record>, Vec<u32>)> {
+fn parse_row(input: &str) -> IResult<&str, (Vec<Record>, Vec<u64>)> {
     separated_pair(
         many1(alt((
             complete::char('#').map(|_| Damaged),
@@ -25,31 +26,36 @@ fn parse_row(input: &str) -> IResult<&str, (Vec<Record>, Vec<u32>)> {
             complete::char('?').map(|_| Unknown),
         ))),
         space1,
-        separated_list1(tag(","), complete::u32),
+        separated_list1(tag(","), complete::u64),
     )(input)
 }
 
-fn parse_rows(input: &str) -> IResult<&str, Vec<(Vec<Record>, Vec<u32>)>> {
+fn parse_rows(input: &str) -> IResult<&str, Vec<(Vec<Record>, Vec<u64>)>> {
     separated_list1(newline, parse_row)(input)
 }
 
 fn calculate_arrangements(
+    cache: &mut HashMap<(Vec<Record>, Vec<u64>), u64>,
     records: Vec<Record>,
-    groups: Vec<u32>,
-) -> u32 {
+    groups: Vec<u64>,
+) -> u64 {
     if records.is_empty() {
         return if groups.is_empty() { 1 } else { 0 };
     }
 
+    if let Some(value) = cache.get(&(records.clone(), groups.clone())) {
+        return *value;
+    }
+
     let permutations = match *records.first().unwrap() {
-        Operational => calculate_arrangements(records[1..].to_vec(), groups),
+        Operational => calculate_arrangements(cache, records[1..].to_vec(), groups.clone()),
         Unknown => {
             let mut with_damaged = records.clone();
             with_damaged[0] = Damaged;
             let mut with_operational = records.clone();
             with_operational[0] = Operational;
-            calculate_arrangements(with_damaged, groups.clone())
-                + calculate_arrangements(with_operational, groups)
+            calculate_arrangements(cache, with_damaged, groups.clone())
+                + calculate_arrangements(cache, with_operational, groups.clone())
         }
         Damaged => {
             if groups.len() == 0 {
@@ -72,13 +78,15 @@ fn calculate_arrangements(
                         }
                     } else if records[want_damaged] == Operational {
                         calculate_arrangements(
+                            cache,
                             records[want_damaged + 1..].to_vec(),
                             new_groups,
-                       )
+                        )
                     } else if records[want_damaged] == Unknown {
                         let mut new_records = records.clone();
                         new_records[want_damaged] = Operational;
                         calculate_arrangements(
+                            cache,
                             new_records[want_damaged..].to_vec(),
                             new_groups,
                         )
@@ -91,15 +99,17 @@ fn calculate_arrangements(
             }
         }
     };
+    cache.insert((records.clone(), groups.clone()), permutations);
     permutations
 }
 
-fn expand_row(records: Vec<Record>, groups: Vec<u32>) -> (Vec<Record>, Vec<u32>) {
-    let new_records = std::iter::repeat(records.into_iter())
-        .take(5)
-        .intersperse(vec![Unknown].into_iter())
-        .flatten()
-        .collect_vec();
+fn expand_row(records: Vec<Record>, groups: Vec<u64>) -> (Vec<Record>, Vec<u64>) {
+    let new_records = itertools::Itertools::intersperse(
+        std::iter::repeat(records.into_iter()).take(5),
+        vec![Unknown].into_iter(),
+    )
+    .flatten()
+    .collect_vec();
     let new_groups = std::iter::repeat(groups.into_iter())
         .take(5)
         .flatten()
@@ -110,19 +120,21 @@ fn expand_row(records: Vec<Record>, groups: Vec<u32>) -> (Vec<Record>, Vec<u32>)
 pub fn process_part1(input: &str) -> String {
     let (input, rows) = parse_rows(input).unwrap();
     debug_assert_eq!(input, "");
+    let mut cache = HashMap::new();
     rows.into_iter()
-        .map(|(records, groups)| calculate_arrangements(records, groups))
-        .sum::<u32>()
+        .map(|(records, groups)| calculate_arrangements(&mut cache, records, groups))
+        .sum::<u64>()
         .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
     let (input, rows) = parse_rows(input).unwrap();
     debug_assert_eq!(input, "");
-    rows.into_par_iter()
+    let mut cache = HashMap::new();
+    rows.into_iter()
         .map(|(records, groups)| expand_row(records, groups))
-        .map(|(records, groups)| calculate_arrangements(records, groups))
-        .sum::<u32>()
+        .map(|(records, groups)| calculate_arrangements(&mut cache, records, groups))
+        .sum::<u64>()
         .to_string()
 }
 
@@ -155,9 +167,13 @@ mod tests {
     #[case("????.######..#####. 1,6,5", 4)]
     #[case("?###???????? 3,2,1", 10)]
     #[trace]
-    fn test_arrangements(#[case] input: &str, #[case] expected: u32) {
+    fn test_arrangements(#[case] input: &str, #[case] expected: u64) {
+        let mut cache = HashMap::new();
         let (_, (records, groups)) = parse_row(input).unwrap();
-        assert_eq!(calculate_arrangements(records, groups), expected);
+        assert_eq!(
+            calculate_arrangements(&mut cache, records, groups),
+            expected
+        );
     }
 
     #[rstest]
